8951a028d6e5954650bfba7bf50b33a243da7634
pedronel <pedronel.builes@gmail.com>
2022-04-09 14:26:21 -0500

    testing of alert component

--- src/components/atoms/alert/alert.test.js
+++ src/components/atoms/alert/alert.test.js
@@ -13,16 +13,61 @@ describe('Testing alert', () => {
     }  
   });
 
-  it('render component alert', () => {
-    
+  it('render only one alert component', () => {
     const component = renderer.create(<AlertPedro type={data.type} message={data.message} />);
 
-    const arbol = component.toTree();
-    const alert = component.root.findAllByType('p');
-  
-    console.log('alert', alert);
-    console.log('arbol', arbol);
-    // expect(alert.textContent).toBe(data.message);
+    const alert = component.toJSON();
+    expect(alert.children.length).toBe(1)
+  })
+
+  it('alert show the message data', () => {
+    const component = renderer.create(<AlertPedro type={data.type} message={data.message} />);
+
+    const alert = component.toJSON();
+    expect(alert.children[0].children[0]).toBe(data.message);
+  })
+
+  it('alert show success class', () => {
+    const component = renderer.create(<AlertPedro type={data.type} message={data.message} />);
+
+    const alert = component.toJSON();
+    expect(alert.props.className.includes(data.type)).toEqual(true)
+  })
+
+  it('alert show info class', () => {
+    data = {
+      ...data,
+      type: 'info'
+    }
+    const component = renderer.create(<AlertPedro type={data.type} message={data.message} />);
+
+    const alert = component.toJSON();
+
+    expect(alert.props.className.includes(data.type)).toEqual(true)
+  })
+
+  it('alert show warning class', () => {
+    data = {
+      ...data,
+      type: 'warning'
+    }
+    const component = renderer.create(<AlertPedro type={data.type} message={data.message} />);
+
+    const alert = component.toJSON();
+    
+    expect(alert.props.className.includes(data.type)).toEqual(true)
+  })
+
+  it('alert show error class', () => {
+    data = {
+      ...data,
+      type: 'error'
+    }
+    const component = renderer.create(<AlertPedro type={data.type} message={data.message} />);
+
+    const alert = component.toJSON();
+    
+    expect(alert.props.className.includes(data.type)).toEqual(true)
   })
 
 })
